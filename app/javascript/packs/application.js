966e2632a61193a0039cf58a5f546b1535075fa2
Shenaj Marla mshenaj@gmail.com <mshenaj@gmail.com>
2020-02-28 11:57:46 +0100

    pick-up time fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,3 +5,15 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import { initMapbox } from '../plugins/map';
 
 initMapbox();
+
+let minutesInput = document.querySelector('input[name="order_item[minutes]"]')
+
+if (minutesInput){
+  minutesInput.addEventListener('change', (e) => {
+    let pickUpTime = new Date(new Date().getTime() + Number(e.target.value)*60000)
+    console.log(pickUpTime)
+    let displayEl = document.querySelector(".pick-up-time")
+    let innerText = "You should pick your candy at: "
+    displayEl.innerText = innerText + pickUpTime.getHours() + ":" + pickUpTime.getMinutes()
+  })
+}
